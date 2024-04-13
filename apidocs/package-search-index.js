0ff5135971cb2576721bbe002c53d341b27e430c
robtimus <robtimus@users.noreply.github.com>
2024-04-13 15:06:28 +0000

    Updated for version 4.0

---
+++ apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.github.robtimus.connect.sdk.java.springboot"},{"l":"com.github.robtimus.connect.sdk.java.springboot.actuator"},{"l":"com.github.robtimus.connect.sdk.java.springboot.autoconfigure"},{"l":"com.github.robtimus.connect.sdk.java.springboot.logging"}];updateSearchResults();
